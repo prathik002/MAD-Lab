aa1760e32e52104557f2cf09656b2543c065ae14
Prathik Vasant <prathikvasant123@gmail.com>
2026-09-07 12:29:37 +0530

    Add files via upload

---
+++ Program6/java/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.explicit_intents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void OnIndexSendButtonClicked(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        EditText editText = findViewById(R.id.edt1);
+        String index = editText.getText().toString();
+        intent.putExtra("ImageIndex",index);
+        startActivity(intent);
+    }
+}
